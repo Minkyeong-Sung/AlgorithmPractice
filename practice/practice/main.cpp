495d300693a6291bd7e14033aa5726befb3a896e
Minkyeong Seong <>
2018-11-18 19:54:54 +0900

    [TestDome]Merge Names

--- practice/practice/main.cpp
+++ practice/practice/main.cpp
@@ -1,37 +1,50 @@
 #include <iostream>
-#include <string>
+#include <vector>
 
 using namespace std;
 
-class TextInput
+class MergeNames
 {
 public:
-    string value;
-    virtual void add(char c) {
-        value+= c;
-    }
-    
-    string getValue() { return value; }
-};
-
-class NumericInput : public TextInput {
-    
-    void add(char c){
+    static vector<string> unique_names(const vector<string>& names1, const vector<string>& names2)
+    {
+        vector<string> vt;
+        int flag = 0;
         
-        if( c >= '0'  && c <= '9'){
-            value += c;
+        for(int i=0; i<names1.size(); i++){
+            vt.push_back(names1[i]);
         }
+        
+        for(int i=0; i< names2.size(); i++){
+            
+            vt.push_back(names2[i]);
+        }
+        
+        // 중복된 이름 모두 삭제
+        for(int i=0; i<vt.size(); i++){
+            for(int j=i+1; j< vt.size(); j++){
+             
+                if(vt[i] == vt[j]){
+                    vt.erase(vt.begin()+j);
+                    
+                }
+            }
+        }
+        
+        return vt;
     }
-    
 };
 
 #ifndef RunTests
 int main()
 {
-    TextInput* input = new NumericInput();
-    input->add('1');
-    input->add('a');
-    input->add('0');
-    std::cout << input->getValue();
+    vector<string> names1 = {"Ava", "Emma", "Olivia"};
+    vector<string> names2 = {"Olivia", "Sophia", "Emma"};
+    
+    vector<string> result = MergeNames::unique_names(names1, names2);
+    for(auto element : result)
+    {
+        cout << element << ' '; // should print Ava Emma Olivia Sophia
+    }
 }
 #endif
